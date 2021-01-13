946234fab44feb15da637b29b53366d27e2d56ac
PedroSpiet <pedro.silva186@fatec.sp.gov.br>
2021-01-13 16:59:22 -0300

    performing queries against the database

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,15 +1,38 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import db.DB;
+import db.DBException;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Connection conn = DB.getconnection();
+		Connection conn = null;
+		Statement st = null;
+		ResultSet rs = null;
+		
+		try {
+			conn = DB.getconnection();
+				st = conn.createStatement();
+				
+				rs = st.executeQuery("SELECT * FROM department");
+				
+				while(rs.next()) {
+					System.out.println("ID:" + rs.getInt("id") + ", NAME:" + rs.getString("name") );
+				}
+			DB.closeConnection();
 			
-		DB.closeConnection();
+		} catch (SQLException e) {
+			throw new DBException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+			DB.closeConnection();
+		}
 	}
 
 }
